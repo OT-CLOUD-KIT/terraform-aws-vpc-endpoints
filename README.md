995f85089c86a9c229a6e1a37e9cbe5eea5ba9a8
faizOT <faizan.khan@opstree.com>
2024-03-18 17:00:49 +0530

    updated module

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # terraform-aws-vpc-enpoints
+
+
+# Example to use module
+
+provider "aws" {
+  region = "us-west-2"  # or specify your desired region
+}
+
+module "my_vpc_endpoint" {
+  source = "./terraform-aws-vpc-enpoints/"  # Path to your module directory or module registry
+  
+  # Pass values to the input variables
+  vpc_id                                           = "your_vpc_id"
+  service_name                                     = "your_service_name"
+  endpoint_name                                    = "your_endpoint_name"
+  route_table_ids                                  = ["rtb-12345678"]  # Example route table ID
+  tags                                             = {
+    "Environment" = "Production"
+    "Owner"       = "YourName"
+  }
+
+  # You can provide values to other optional variables as needed
+  auto_accept                                      = false
+  private_dns_enabled                              = true
+  dns_options                                      = {}
+  ip_address_type                                  = "ipv4"
+  subnet_ids                                       = ["subnet-12345678"]  # Example subnet ID
+  security_group_ids                               = ["sg-12345678"]  # Example security group ID
+  dns_record_ip_type                               = "A"
+  private_dns_only_for_inbound_resolver_endpoint  = false
+}
--- variable.tf
+++ variable.tf
@@ -1,11 +1,7 @@
-
 variable "vpc_id" {
   type        = string
-  default     = ""
   description = "vpc id"
 }
-
-####################### endpoint ###################
 
 variable "service_name" {
   type        = string
@@ -15,14 +11,13 @@ variable "service_name" {
 variable "endpoint_type" {
   type        = string
   default     = "Gateway"
-  description = " endpoint type"
+  description = "endpoint type"
 }
 
 variable "route_table_ids" {
   type        = list(any)
   description = "route table id for endpoint"
 }
-
 
 variable "endpoint_policy" {
   type        = string
@@ -53,3 +48,50 @@ variable "tags" {
   default     = {}
 }
 
+variable "auto_accept" {
+  type        = bool
+  default     = false
+  description = "Whether or not to automatically accept endpoint connections"
+}
+
+variable "private_dns_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether or not to enable private DNS for the endpoint"
+}
+
+variable "dns_options" {
+  type        = map(string)
+  default     = {}
+  description = "DNS options for the endpoint"
+}
+
+variable "ip_address_type" {
+  type        = string
+  default     = "ipv4"
+  description = "The IP address type to use for the endpoint"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "List of subnet IDs in which to create the endpoint network interfaces"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  default     = []
+  description = "List of security group IDs to associate with the endpoint network interfaces"
+}
+
+variable "dns_record_ip_type" {
+  type        = string
+  default     = "A"
+  description = "The IP type for the DNS record"
+}
+
+variable "private_dns_only_for_inbound_resolver_endpoint" {
+  type        = bool
+  default     = false
+  description = "Whether or not to enable private DNS only for inbound resolver endpoint"
+}
